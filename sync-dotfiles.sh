eb528e5f98ed77f2c87aa29162673713401d61d3
Jorge Luis Hernández <shadday007@gmail.com>
2022-11-17 08:37:14 -0400

    feat: Reduce complexity

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -7,50 +7,61 @@ fi
 # Navigate to the directory of this script 
 pushd $STOW_DIRECTORY
 
-if [[ -z $STOW_PACKAGES ]]; then
-	readarray -t STOW_PACKAGES < <(for i in $(ls -d [!^.]*/); do echo ${i%%/}; done)
-fi
-
 # Sync dotfiles repo and ensure that dotfiles are tangled correctly afterward
 GREEN='\033[1;32m'
 BLUE='\033[1;34m'
 RED='\033[1;30m'
 NC='\033[0m'
 
-echo -e "${BLUE}Stashing existing changes in dotfiles...${NC}"
-stash_result=$(git checkout master;git stash push -m "sync-dotfiles: Before syncing dotfiles")
-echo -e "${BLUE}Stashing existing changes in submodules...${NC}"
-stash_result=$(git submodule foreach 'git checkout master; git stash')
+# Get the name of the current Git branch to check is master
+# branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+branch=$(git symbolic-ref --short HEAD)
 
-echo -e "${BLUE}Pulling updates from dotfiles repo...${NC}"
+if [ "$branch" == "master" ]
+then
+	echo "Compiling and deploying $branch to production..."
+else
+	echo "Will not publish from a branch other than master to production.\nPlease merge your changes into master and try again.\n\n"
+fi
+
+# Save changes on actual branches
+echo -e "${BLUE}Stashing existing changes in dotfiles...${NC}"
+git stash push -m "sync-dotfiles: Before syncing dotfiles"
+echo -e "${BLUE}Stashing existing changes in submodules...${NC}"
+git submodule foreach 'git stash'
+
+# Switch to master branches
+echo -e "${BLUE}Switch to master...${NC}"
+git checkout master
+git submodule foreach 'git checkout master'
+
+# Pulling all changes
+echo -e "${BLUE}Pulling updates from dotfiles repo and submodules...${NC}"
 git pull origin master
-echo -e "${BLUE}Pulling updates from submodules...${NC}"
 git submodule foreach 'git pull origin master'
+
+# TODO: decide if these variables are fixed or dynamics
+if [[ -z $STOW_PACKAGES ]]; then
+	readarray -t STOW_PACKAGES < <(for i in $(ls -d [!^.]*/); do echo ${i%%/}; done)
+fi
+
+# Run stow to ensure all new dotfiles are linked
+echo -e "${GREEN}Run stow to ensure all new dotfiles are linked...${NC}"
+for folder in "${STOW_PACKAGES[@]}"
+do
+	echo "stow $folder"
+	stow -D $folder
+	stow $folder
+done
+
+echo -e "${BLUE}Switch back to old branch...${NC}"
+stash_result=$(git checkout -)
+stash_result=$(git submodule foreach 'git checkout -')
 
 echo -e "${BLUE}Popping stashed changes...${NC}"
 git stash pop
 echo -e "${BLUE}Popping stashed changes in submodules...${NC}"
 git submodule foreach 'git stash pop || :'
-
-echo -e "${BLUE}finding unmerged files in dotfiles repo...${NC}"
-unmerged_files=$(git diff --name-only --diff-filter=U)
-echo -e "${BLUE}finding unmerged files in submodules...${NC}"
-git submodule foreach 'unmerged_files+=$(git diff --name-only --diff-filter=U)'
-
-if [[ ! -z $unmerged_files ]]; then
-	echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
-	echo
-	printf %"s\n" $unmerged_files  # Ensure newlines are printed
-else
-	# Run stow to ensure all new dotfiles are linked
-	echo -e "${GREEN}Run stow to ensure all new dotfiles are linked...${NC}"
-	for folder in "${STOW_PACKAGES[@]}"
-	do
-		echo "stow $folder"
-		stow -D $folder
-		stow $folder
-	done
-fi
 
 popd
 
